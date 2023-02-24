b4c3bbaaf6b16bc6ef2051e87a56658f6a6f1a3a
Jim_Mutombo <68845313+jimmutombo@users.noreply.github.com>
2023-02-24 16:41:41 +0200

    test

--- data.js
+++ data.js
@@ -1,27 +1,52 @@
-const form = document.getElementById('employee-form');
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-form.addEventListener('submit', function(e) {
-	e.preventDefault();
-	
-	const name = document.getElementById('name').value;
-	const email = document.getElementById('email').value;
-	const dob = document.getElementById('dob').value;
-	const address = document.getElementById('address').value;
-	const department = document.getElementById('department').value;
+var firebaseConfig = {
+	apiKey: "AIzaSyDF5LqLHUSN0kWYnXNeqHrFprl14Ggtg80",
+    authDomain: "client-registration-3fbf8.firebaseapp.com",
+    databaseURL: "https://client-registration-3fbf8-default-rtdb.firebaseio.com",
+    projectId: "client-registration-3fbf8",
+    storageBucket: "client-registration-3fbf8.appspot.com",
+    messagingSenderId: "1069322992988",
+    appId: "1:1069322992988:web:cd1a35244b6ae300241599"
+};
+firebase.initializeApp(firebaseConfig);
 
-	const employee = {
-		name,
-		email,
-		dob,
-		address,
-		department
-	};
+// Get a reference to the database service
+var  clientRegistration = firebase.database().ref ("record-form");
 
-	// Here, you can store the employee data in a database or send it to a server.
+// Get a reference to the client registration form
+var recordForm = document.getElementById("record-form");
+document.getElementById("record-form").addEventListener("submit", submitForm);
 
-	alert('Employee registration successful!');
-	form.reset();
-    window.open(`employee_records.html?name=${name}&email=${email}&dob=${dob}&address=${address}&department=${department}`);
+  // Get the form data
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var dob = document.getElementById("dob").value;
+  var address = document.getElementById("address").value;
+  var department = document.getElementById("department").value;
+
+  // Create a new client registration object
+  var newClientRegistration = {
+    name: name,
+    email: email,
+    dob: dob,
+    address: address,
+    department: department
+  };
+
+  // Get a new key for the client registration object
+  var newClientRegistrationKey = database.ref().child("CLIENT-REGISTRATION").push().key;
+
+  // Save the client registration object to the database
+  var updates = {};
+  updates["/CLIENT-REGISTRATION/" + newClientRegistrationKey] = newClientRegistration;
+  database.ref().update(updates);
+
+  // Clear the form
+  form.reset();
+
+  // Display a success message to the user
+  alert("Client registration saved successfully!");
 });
-
 
